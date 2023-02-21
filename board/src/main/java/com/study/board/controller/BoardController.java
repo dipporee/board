d8246bab97c78eb40ae8704e12fa43453c05bcec
dipporee <dipporee@gmail.com>
2023-02-22 01:33:52 +0900

    디포리 커밋

--- board/src/main/java/com/study/board/controller/BoardController.java
+++ board/src/main/java/com/study/board/controller/BoardController.java
@@ -29,6 +29,7 @@ public class BoardController {
 		log.info("BoardController list()");
 		log.info("main에 커밋하기");
 		log.info("디포리");
+		log.info("디포리2");
 	    log.info("boardService.getList() : " + boardService.getList());
 	    
 		model.addAttribute("list", boardService.getList());
